b834840243b5416c1196093d2e0c973abc881349
Vu Tran Kien <vutrankien.vn@gmail.com>
2017-08-06 12:31:20 +0900

    Revert to using TableLayout for dialpad

--- app/src/main/java/com/vutrankien/t9vietnamese/T9Vietnamese.java
+++ app/src/main/java/com/vutrankien/t9vietnamese/T9Vietnamese.java
@@ -1,7 +1,11 @@
 package com.vutrankien.t9vietnamese;
 
+import android.annotation.SuppressLint;
 import android.inputmethodservice.InputMethodService;
+import android.support.percent.PercentRelativeLayout;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TableLayout;
 
 /**
  * Created by vutrankien on 17/05/02.
@@ -9,9 +13,15 @@ import android.view.View;
 public class T9Vietnamese extends InputMethodService {
     @Override
     public View onCreateInputView() {
-        T9KeyboardView inputView = (T9KeyboardView) getLayoutInflater().inflate(
-                R.layout.input, null);
-        inputView.setKeyboard(new T9Keyboard(this, R.xml.t9));
+        //T9KeyboardView inputView = (T9KeyboardView) getLayoutInflater().inflate(
+        //        R.layout.input, null);
+        //inputView.setKeyboard(new T9Keyboard(this, R.xml.t9));
+        @SuppressLint("InflateParams") @SuppressWarnings("UnnecessaryLocalVariable") TableLayout inputView = (TableLayout) getLayoutInflater().inflate(
+                R.layout.dialpad_table, null);
         return inputView;
+    }
+    @SuppressWarnings("unused")
+    public void onBtnClick(View view) {
+        new KLog("T9Vietnamese").d("onBtnClick() btn=" + ((Button) view).getText());
     }
 }
