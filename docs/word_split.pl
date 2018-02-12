97939219c92babc9abf442e0b4d1bdbc42279777
Vu Tran Kien <vutrankien.vn@gmail.com>
2018-02-12 21:31:54 +0900

    Created word_split.pl to generate morphemes list
    Added more word but the engine is now case-sensitive so not all word will appear

--- docs/word_split.pl
+++ docs/word_split.pl
@@ -1,8 +1,9 @@
+use v5.10;
 use warnings;
 use strict;
-use v5.10;
 use utf8;
 use Unicode::Collate;
+use Getopt::Long;
 
 binmode(STDIN, ":encoding(UTF-8)") || die "can't binmode to utf-8: $!";
 binmode STDOUT, ":encoding(UTF-8) :utf8"
@@ -10,30 +11,47 @@ binmode STDOUT, ":encoding(UTF-8) :utf8"
 binmode STDERR, ":utf8"
         || die "can't binmode to UTF–8: $!";
 
+Getopt::Long::Configure ("bundling");
+my @inputs;
+my $output;
+
+GetOptions('input|i=s' => \@inputs, 'output|o=s' => \$output);
+#GetOptions();
+
 my %words;
-open WORD, "< :encoding(UTF-8)", "morphemes.txt";
-while (<WORD>) {
-#    print;
-    chomp;
-    $words{$_} = undef;
-}
-close(WORD);
 
-while (<>) {
-   next if /^#/;
-   my @f=split /\s+/;
-   for (@f) {
-       if (/[-\w]+/) {
-           if (!exists $words{$&}) {
-               $words{$&} = undef;
-               say STDERR "Added $&"
-           }
-       }
-   }
+for (@inputs) {
+    my $inputfh;
+    if ($_ eq '-') {
+        $inputfh = *STDIN;
+    } else {
+        open $inputfh, "< :encoding(UTF-8)", $_ or die "can't open $_: $!";
+    }
+
+    while (<$inputfh>) {
+        next if /^#/;
+        chomp;
+        my @f=split /\s+/;
+        for (@f) {
+            if (/[-\w]+/) {
+                if (!exists $words{$&}) {
+                    $words{$&} = undef;
+                    say STDERR "Added $&"
+                }
+            }
+        }
+    }
 }
 
-for (sort keys %words) {
+my $outfh;
+if ($output eq '-') {
+    $outfh = *STDOUT;
+} else {
+    open $outfh, '> :utf8', $output;
+}
+#for (sort keys %words) {
+for (Unicode::Collate->new->sort(keys %words)) {
 #    print "$_\n"
-    say;
+    say $outfh $_;
 #    say "$_ => $words{$_}"
 }
