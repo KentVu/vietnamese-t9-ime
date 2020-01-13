b8b2e2fc7953a621ab82c9af902635fc31b84cd6
Vu Tran Kien <vutrankien.vn@gmail.com>
2020-01-13 18:31:47 +0900

    WIP ProgressiveReaderTests #10
    - Refactorings.

--- lib/src/main/java/com/vutrankien/t9vietnamese/ProgressiveReader.kt
+++ lib/src/main/java/com/vutrankien/t9vietnamese/ProgressiveReader.kt
@@ -3,21 +3,16 @@ package com.vutrankien.t9vietnamese
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.InternalCoroutinesApi
-import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.channels.ReceiveChannel
 import kotlinx.coroutines.channels.produce
-import kotlinx.coroutines.channels.toChannel
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.FlowCollector
-import kotlinx.coroutines.flow.flow
 import java.io.InputStream
 
-class ProgressiveReader(inputStream: InputStream, size: Int) {
-    @ExperimentalCoroutinesApi
-    fun start(scope: CoroutineScope = GlobalScope): ReceiveChannel<Progress> = scope.produce {
-        TODO()
-    }
-
-    data class Progress(val bytesRead: Int, val line: String)
+/**
+ * @param size estimated size of this stream, in bytes.
+ */
+@ExperimentalCoroutinesApi
+fun InputStream.start(size: Int, scope: CoroutineScope = GlobalScope): ReceiveChannel<Progress> = scope.produce {
+    TODO()
 }
+
+data class Progress(val bytesRead: Int, val line: String)
--- lib/src/test/java/com/vutrankien/t9vietnamese/tests/ProgressiveReaderTests.kt
+++ lib/src/test/java/com/vutrankien/t9vietnamese/tests/ProgressiveReaderTests.kt
@@ -1,6 +1,7 @@
 package com.vutrankien.t9vietnamese.tests
 
-import com.vutrankien.t9vietnamese.ProgressiveReader
+import com.vutrankien.t9vietnamese.Progress
+import com.vutrankien.t9vietnamese.start
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.channels.toList
 import kotlinx.coroutines.runBlocking
@@ -14,12 +15,11 @@ class ProgressiveReaderTests {
     fun basicFunction() = runBlocking {
         val content = "a\nb\nc"
         val bytes = content.toByteArray()
-        val progresses = ProgressiveReader(
-            ByteArrayInputStream(bytes),
-            bytes.size
-        ).start(this@runBlocking).toList()
-        assertEquals(progresses[0], ProgressiveReader.Progress(2, "a"))
-        assertEquals(progresses[1], ProgressiveReader.Progress(4, "b"))
-        assertEquals(progresses[2], ProgressiveReader.Progress(5, "c"))
+        val progresses = ByteArrayInputStream(bytes).start(
+                bytes.size,
+                this@runBlocking).toList()
+        assertEquals(progresses[0], Progress(2, "a"))
+        assertEquals(progresses[1], Progress(4, "b"))
+        assertEquals(progresses[2], Progress(5, "c"))
     }
 }
