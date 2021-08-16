2a0a68e015c6d8b2dc754070bf38337bc89e48da
Vu Tran Kien <vutrankien.vn@gmail.com>
2021-08-16 11:04:39 +0900

    [WIP] Extracting AndroidView

--- app/src/main/java/com/vutrankien/t9vietnamese/android/MainActivity.kt
+++ app/src/main/java/com/vutrankien/t9vietnamese/android/MainActivity.kt
@@ -1,25 +1,19 @@
 package com.vutrankien.t9vietnamese.android
 
 import android.app.Activity
-import android.content.Context
 import android.content.Intent
-import android.inputmethodservice.KeyboardView
 import android.os.Bundle
-import android.os.PowerManager
 import android.provider.Settings
 import android.view.KeyEvent
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
 import android.view.inputmethod.EditorInfo
-import android.view.inputmethod.InputConnection
 import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import androidx.annotation.VisibleForTesting
-import androidx.core.content.ContextCompat
-import androidx.recyclerview.widget.RecyclerView
 import com.vutrankien.t9vietnamese.engine.DefaultT9Engine
 import com.vutrankien.t9vietnamese.lib.*
 import kotlinx.coroutines.*
@@ -36,115 +30,9 @@ class MainActivity : Activity() {
 
     private val channel: Channel<EventWithData<Event, Key>> = Channel()
 
+    private val preferences by lazy { Preferences(applicationContext) }
+
     private lateinit var view: MainActivityView
-
-    class MainActivityView(
-        val logFactory: LogFactory,
-        context: Context,
-        override val scope: CoroutineScope,
-        channel: Channel<EventWithData<Event, Key>>,
-        private val textView: TextView,
-        internal val inputConnection: InputConnection
-    ) : AndroidView(context, scope, channel) {
-
-        companion object {
-            private const val WAKELOCK_TIMEOUT = 60000L
-        }
-
-        private val log = logFactory.newLog("MainActivity.V")
-
-        private val eventSink: SendChannel<EventWithData<Event, Key>> = channel
-
-        private lateinit var wakelock: PowerManager.WakeLock
-
-        fun init(kbView: KeyboardView, candidatesView: RecyclerView) {
-            kbView.keyboard = T9Keyboard(context)
-            kbView.setOnKeyboardActionListener(
-                KeyboardActionListener(
-                    logFactory,
-                    scope,
-                    eventSink
-                )
-            )
-            logic.initializeCandidatesView(candidatesView)
-            val powerManager = context.getSystemService(POWER_SERVICE) as PowerManager
-            wakelock = powerManager.newWakeLock(
-                PowerManager.SCREEN_DIM_WAKE_LOCK,
-                "${BuildConfig.APPLICATION_ID}:MainActivity")
-
-            wakelock.acquire(WAKELOCK_TIMEOUT)
-            scope.launch {
-                eventSink.send(Event.START.noData())
-            }
-        }
-
-        fun onBtnClick(key: Char) {
-            log.d("onBtnClick() key=$key")
-            scope.launch {
-                eventSink.send(Event.KEY_PRESS.withData(Key.fromChar(key)))
-            }
-        }
-
-        fun onKeyDown(event: KeyEvent?) {
-            if (event == null) {
-                log.e("onKeyDown:event is null!")
-                return
-            }
-            val char = event.unicodeChar.toChar()
-            log.d("onKeyDown:num=$char")
-            scope.launch {
-                eventSink.send(Event.KEY_PRESS.withData(Key.fromChar(char)))
-            }
-        }
-
-        private fun displayInfo(resId: Int, vararg formatArgs: Any) {
-            textView.setTextColor(ContextCompat.getColor(context, android.R.color.holo_green_dark))
-            textView.text = context.getString(resId, *formatArgs)
-        }
-
-        private fun displayError(msg: String) {
-            val color = ContextCompat.getColor(context, android.R.color.holo_red_dark)
-            textView.setTextColor(color)
-            textView.text = context.getString(R.string.oops, msg)
-        }
-
-        private fun displayError(e: Exception) {
-            displayError(e.message ?: "")
-        }
-
-        override fun showKeyboard() {
-            log.w("View: TODO: showKeyboard")
-            wakelock.run { if(isHeld) release() }
-            displayInfo(R.string.notify_initialized)
-            //defaultSharedPreferences.edit().putLong("load_time", loadTime).apply()
-        }
-
-        override fun showCandidates(candidates: Collection<String>) {
-            log.d("View: showCandidates:$candidates")
-            logic.updateCandidates(candidates)
-            //testingHook.onShowCandidates()
-        }
-
-        override fun candidateSelected(selectedCandidate: Int) {
-            logic.selectCandidate(selectedCandidate)
-        }
-
-        override fun confirmInput(word: String) {
-            log.d("View: confirmInput($word)")
-            inputConnection.commitText(word, 1)
-            logic.clearCandidates()
-        }
-
-        override fun deleteBackward() {
-            log.d("deleteBackward")
-            inputConnection.deleteSurroundingText(1, 0)
-//        findViewById<MyEditText>(R.id.editText).apply {
-//            inputConnection!!.deleteSurroundingText(1, 0)
-//        }
-//        findViewById<EditText>(R.id.editText).onCreateInputConnection()
-        }
-
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -243,24 +131,5 @@ class MainActivity : Activity() {
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         log.d("onActivityResult:$requestCode:res=$resultCode:data=$data")
         super.onActivityResult(requestCode, resultCode, data)
-    }
-
-    interface TestingHook {
-        val candidatesAdapter: WordListAdapter
-        val eventSink: SendChannel<EventWithData<Event, Key>>
-
-        //fun waitNewCandidates()
-    }
-
-    /** For integration testing. */
-    @VisibleForTesting
-    val testingHook = object: TestingHook {
-        override val candidatesAdapter: WordListAdapter
-            //get() = this@MainActivity.findViewById<RecyclerView>(R.id.candidates_view).adapter as WordListAdapter
-            get() = (this@MainActivity.logic as UiLogic.DefaultUiLogic).wordListAdapter
-        override val eventSink = this@MainActivity.eventSink
-        //override fun waitNewCandidates() {
-        //    this@MainActivity.
-        //}
     }
 }
--- app/src/main/java/com/vutrankien/t9vietnamese/android/T9Keyboard.kt
+++ app/src/main/java/com/vutrankien/t9vietnamese/android/T9Keyboard.kt
@@ -1,19 +1,9 @@
-package com.vutrankien.t9vietnamese.android;
+package com.vutrankien.t9vietnamese.android
 
-import android.content.Context;
-import android.inputmethodservice.Keyboard;
+import android.content.Context
+import android.inputmethodservice.Keyboard
 
 /**
  * Created by vutrankien on 17/07/24.
  */
-
-class T9Keyboard extends Keyboard {
-    T9Keyboard(Context context) {
-        this(context, R.xml.t9);
-    }
-
-    T9Keyboard(Context context, int xmlLayoutResId) {
-        super(context, xmlLayoutResId);
-    }
-
-}
+internal class T9Keyboard @JvmOverloads constructor(context: Context?, xmlLayoutResId: Int = R.xml.t9) : Keyboard(context, xmlLayoutResId)
