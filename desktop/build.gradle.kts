170060e494d2948ab71b5e872cba5a0520c47fdd
Vu Tran Kien <vutrankien.vn@gmail.com>
2022-11-20 02:50:32 +0900

    Added desktop module

---
+++ desktop/build.gradle.kts
@@ -0,0 +1,20 @@
+plugins {
+    kotlin("jvm")// version "1.7.20"
+    id("org.jetbrains.compose")// version "1.2.1"
+}
+
+//repositories {
+//    mavenCentral()
+//    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+//    google()
+//}
+
+dependencies {
+    implementation(compose.desktop.currentOs)
+}
+
+compose.desktop {
+    application {
+        mainClass = "MainKt"
+    }
+}
